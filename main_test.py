30d10b3c939261543134804c616071c80ff6e88c
dian yang <2113591422@qq.com>
2021-12-14 12:00:15 +0800

    changed the name

--- main_test.py
+++ main_test.py
@@ -1,16 +1,15 @@
 import unittest
-from main import print_hi_stephen, print_hi_you#, print_hi_?
-
+from main import print_hi_stephen, print_hi_Kyanna, print_hi_Cindy  # , print_hi_
 
 class MyTestCase(unittest.TestCase):
     def test_print_hi_stephen(self):
         self.assertEqual(print_hi_stephen(), f"Hi, Stephen")
 
     def test_print_hi_you(self):
-        self.assertEqual(print_hi_you(), f"")
+        self.assertEqual(print_hi_Kyanna(), f"Hi, the most beautiful girl!!!!")
 
-    #def test_print_hi_?(self):
-        #
+    def test_print_hi_Cindy(self):
+        self.assertEqual(print_hi_Cindy(), f"Hi, Cindybb!!")
 
 
 if __name__ == '__main__':
